6f9d8c63e6bf96d64be8f4d6b6ce6b143e032cb6
androosk <atirpok3@gmail.com>
2019-07-08 15:52:15 -0400

    sigh

--- server.js
+++ server.js
@@ -1,18 +1,16 @@
-require("dotenv").config();
-import express, { static, urlencoded, json } from 'express';
-import exphbs from 'express-handlebars';
-import { connect } from 'mongoose';
+const express = require('express')
+const exphbs = require('express-handlebars')
+const mongoose = require('mongoose')
 
 
-import routes from './controllers/scraper_controller.js';
+const routes = require('./controllers/scraper_controller.js')
 
 const PORT = process.env.PORT || 3000
 const app = express()
 
-app.use(morgan('dev'))
-app.use(static('public'))
-app.use(urlencoded({ extended: true}))
-app.use(json())
+app.use(express.static('public'))
+app.use(express.urlencoded({ extended: true}))
+app.use(express.json())
 
 // app.use(methodOverride('_method'))
 
@@ -20,7 +18,7 @@ app.engine('handlebars', exphbs({ defaultLayout: 'main'}))
 app.set('view engine', 'handlebars')
 
 const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/newYorkerScraper'
-connect(MONGODB_URI, {useNewUrlParser: true})
+mongoose.connect(MONGODB_URI, {useNewUrlParser: true})
 
 app.use('/', routes)
 
